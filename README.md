038e5169ce873a45bae5ba334eee45e3845d98ad
Nathan <nathan.agez@epitech.eu>
2022-01-29 10:41:05 +0100

    chore(*): base commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# amazon-dynamodb-standard-ia
+
+That example provides functionality that will help to move your data from Amazon DynamoDB Standard to DynamoDB Standard-Infrequent Access table class using DynamoDB Time to Live (TTL) and DynamoDB Streams.
+
+## Useful commands
+
+ * `npm run build`   compile typescript to js
+ * `npm run watch`   watch for changes and compile
+ * `npm run test`    perform the jest unit tests
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk synth`       emits the synthesized CloudFormation template
---
+++ test/amazon-dynamodb-standard-ia.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmazonDynamodbStandardIa from '../lib/amazon-dynamodb-standard-ia-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amazon-dynamodb-standard-ia-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmazonDynamodbStandardIa.AmazonDynamodbStandardIaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
